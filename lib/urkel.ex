8c36b1913fcc93536aaf18634da44bd78e707520
Jordon Smith <jrd@mockra.net>
2014-10-05 20:04:35 +0100

    Formatting changes

--- lib/urkel.ex
+++ lib/urkel.ex
@@ -7,9 +7,17 @@ defmodule Urkel do
     import Supervisor.Spec
 
     children = [
-        worker(Urkel.Irc.Connection, [[Urkel.Plugin.Pong, Urkel.Plugin.Admin, Urkel.Plugin.Title]])
+      worker(Urkel.Irc.Connection, [plugins])
     ]
 
     {:ok, _} = Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  defp plugins do
+    [
+      Urkel.Plugin.Pong,
+      Urkel.Plugin.Admin,
+      Urkel.Plugin.Title
+    ]
   end
 end
--- lib/urkel/plugin/admin.ex
+++ lib/urkel/plugin/admin.ex
@@ -1,14 +1,24 @@
 defmodule Urkel.Plugin.Admin do
   use Urkel.Plugin.Mixin
 
-  def handle(pid, msg = %Message{command: "PRIVMSG", trailing: text, prefix: %Prefix{host: "C86A9EBA.D4828085.69B3A1C0.IP"}}) do
+  @admin %Prefix{host: "C86A9EBA.D4828085.69B3A1C0.IP"}
+
+  def handle(pid, msg = %Message{
+    command: "PRIVMSG",
+    trailing: text,
+    prefix: admin
+  }) do
     cond do
       text =~ ~r/^:join/ ->
         join(pid, text |> String.split_at(6) |> elem(1))
       text =~ ~r/^:part/ ->
         part(pid, text |> String.split_at(6) |> elem(1))
       text =~ ~r/^:echo/ ->
-        privmsg(pid, msg |> Irc.get_target, text |> String.split_at(6) |> elem(1))
+        privmsg(
+          pid,
+          msg |> Irc.get_target,
+          text |> String.split_at(6) |> elem(1)
+        )
       true -> nil
     end
   end
